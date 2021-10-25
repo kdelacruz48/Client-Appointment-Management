5c018c1e819b55084e03a694e3f0e8d59d1a41ce
Kyle Delacruz <k.delacruz48.kd@gmail.com>
2021-10-25 00:56:54 +0000

    added delete to customer, still need input validation

--- KyleDelacruzc969/Pages/Main.Designer.cs
+++ KyleDelacruzc969/Pages/Main.Designer.cs
@@ -29,171 +29,172 @@ namespace KyleDelacruzc969.Pages
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.AppointmentsLabel = new System.Windows.Forms.Label();
-			this.CustomersLabel = new System.Windows.Forms.Label();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.dgvCustomers = new System.Windows.Forms.DataGridView();
-			this.buttonCalender = new System.Windows.Forms.Button();
-			this.buttonReports = new System.Windows.Forms.Button();
-			this.buttonLogOut = new System.Windows.Forms.Button();
-			this.buttonAddA = new System.Windows.Forms.Button();
-			this.buttonModifyA = new System.Windows.Forms.Button();
-			this.buttonDeleteA = new System.Windows.Forms.Button();
-			this.buttonAddC = new System.Windows.Forms.Button();
-			this.buttonModifyC = new System.Windows.Forms.Button();
-			this.buttonDeleteC = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// AppointmentsLabel
-			// 
-			this.AppointmentsLabel.AutoSize = true;
-			this.AppointmentsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.AppointmentsLabel.Location = new System.Drawing.Point(30, 9);
-			this.AppointmentsLabel.Name = "AppointmentsLabel";
-			this.AppointmentsLabel.Size = new System.Drawing.Size(120, 20);
-			this.AppointmentsLabel.TabIndex = 0;
-			this.AppointmentsLabel.Text = "Appointments";
-			// 
-			// CustomersLabel
-			// 
-			this.CustomersLabel.AutoSize = true;
-			this.CustomersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.CustomersLabel.Location = new System.Drawing.Point(30, 251);
-			this.CustomersLabel.Name = "CustomersLabel";
-			this.CustomersLabel.Size = new System.Drawing.Size(95, 20);
-			this.CustomersLabel.TabIndex = 1;
-			this.CustomersLabel.Text = "Customers";
-			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(23, 32);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(735, 190);
-			this.dataGridView1.TabIndex = 2;
-			// 
-			// dgvCustomers
-			// 
-			this.dgvCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-			this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dgvCustomers.Location = new System.Drawing.Point(23, 274);
-			this.dgvCustomers.MultiSelect = false;
-			this.dgvCustomers.Name = "dgvCustomers";
-			this.dgvCustomers.ReadOnly = true;
-			this.dgvCustomers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.dgvCustomers.Size = new System.Drawing.Size(599, 173);
-			this.dgvCustomers.TabIndex = 3;
-			// 
-			// buttonCalender
-			// 
-			this.buttonCalender.Location = new System.Drawing.Point(628, 274);
-			this.buttonCalender.Name = "buttonCalender";
-			this.buttonCalender.Size = new System.Drawing.Size(160, 46);
-			this.buttonCalender.TabIndex = 4;
-			this.buttonCalender.Text = "Calender";
-			this.buttonCalender.UseVisualStyleBackColor = true;
-			// 
-			// buttonReports
-			// 
-			this.buttonReports.Location = new System.Drawing.Point(628, 339);
-			this.buttonReports.Name = "buttonReports";
-			this.buttonReports.Size = new System.Drawing.Size(160, 46);
-			this.buttonReports.TabIndex = 5;
-			this.buttonReports.Text = "Reports";
-			this.buttonReports.UseVisualStyleBackColor = true;
-			// 
-			// buttonLogOut
-			// 
-			this.buttonLogOut.Location = new System.Drawing.Point(628, 401);
-			this.buttonLogOut.Name = "buttonLogOut";
-			this.buttonLogOut.Size = new System.Drawing.Size(160, 46);
-			this.buttonLogOut.TabIndex = 6;
-			this.buttonLogOut.Text = "Log Out";
-			this.buttonLogOut.UseVisualStyleBackColor = true;
-			this.buttonLogOut.Click += new System.EventHandler(this.buttonLogOut_Click);
-			// 
-			// buttonAddA
-			// 
-			this.buttonAddA.Location = new System.Drawing.Point(244, 228);
-			this.buttonAddA.Name = "buttonAddA";
-			this.buttonAddA.Size = new System.Drawing.Size(75, 39);
-			this.buttonAddA.TabIndex = 7;
-			this.buttonAddA.Text = "Add";
-			this.buttonAddA.UseVisualStyleBackColor = true;
-			// 
-			// buttonModifyA
-			// 
-			this.buttonModifyA.Location = new System.Drawing.Point(335, 228);
-			this.buttonModifyA.Name = "buttonModifyA";
-			this.buttonModifyA.Size = new System.Drawing.Size(75, 39);
-			this.buttonModifyA.TabIndex = 8;
-			this.buttonModifyA.Text = "Modify";
-			this.buttonModifyA.UseVisualStyleBackColor = true;
-			// 
-			// buttonDeleteA
-			// 
-			this.buttonDeleteA.Location = new System.Drawing.Point(428, 228);
-			this.buttonDeleteA.Name = "buttonDeleteA";
-			this.buttonDeleteA.Size = new System.Drawing.Size(75, 39);
-			this.buttonDeleteA.TabIndex = 9;
-			this.buttonDeleteA.Text = "Delete";
-			this.buttonDeleteA.UseVisualStyleBackColor = true;
-			// 
-			// buttonAddC
-			// 
-			this.buttonAddC.Location = new System.Drawing.Point(244, 453);
-			this.buttonAddC.Name = "buttonAddC";
-			this.buttonAddC.Size = new System.Drawing.Size(75, 39);
-			this.buttonAddC.TabIndex = 10;
-			this.buttonAddC.Text = "Add";
-			this.buttonAddC.UseVisualStyleBackColor = true;
-			this.buttonAddC.Click += new System.EventHandler(this.buttonAddC_Click);
-			// 
-			// buttonModifyC
-			// 
-			this.buttonModifyC.Location = new System.Drawing.Point(335, 453);
-			this.buttonModifyC.Name = "buttonModifyC";
-			this.buttonModifyC.Size = new System.Drawing.Size(75, 39);
-			this.buttonModifyC.TabIndex = 11;
-			this.buttonModifyC.Text = "Modify";
-			this.buttonModifyC.UseVisualStyleBackColor = true;
-			// 
-			// buttonDeleteC
-			// 
-			this.buttonDeleteC.Location = new System.Drawing.Point(428, 453);
-			this.buttonDeleteC.Name = "buttonDeleteC";
-			this.buttonDeleteC.Size = new System.Drawing.Size(75, 39);
-			this.buttonDeleteC.TabIndex = 12;
-			this.buttonDeleteC.Text = "Delete";
-			this.buttonDeleteC.UseVisualStyleBackColor = true;
-			// 
-			// Main
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 498);
-			this.Controls.Add(this.buttonDeleteC);
-			this.Controls.Add(this.buttonModifyC);
-			this.Controls.Add(this.buttonAddC);
-			this.Controls.Add(this.buttonDeleteA);
-			this.Controls.Add(this.buttonModifyA);
-			this.Controls.Add(this.buttonAddA);
-			this.Controls.Add(this.buttonLogOut);
-			this.Controls.Add(this.buttonReports);
-			this.Controls.Add(this.buttonCalender);
-			this.Controls.Add(this.dgvCustomers);
-			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.CustomersLabel);
-			this.Controls.Add(this.AppointmentsLabel);
-			this.Name = "Main";
-			this.Text = "Main";
-			this.Load += new System.EventHandler(this.Main_Load);
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.AppointmentsLabel = new System.Windows.Forms.Label();
+            this.CustomersLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.buttonCalender = new System.Windows.Forms.Button();
+            this.buttonReports = new System.Windows.Forms.Button();
+            this.buttonLogOut = new System.Windows.Forms.Button();
+            this.buttonAddA = new System.Windows.Forms.Button();
+            this.buttonModifyA = new System.Windows.Forms.Button();
+            this.buttonDeleteA = new System.Windows.Forms.Button();
+            this.buttonAddC = new System.Windows.Forms.Button();
+            this.buttonModifyC = new System.Windows.Forms.Button();
+            this.buttonDeleteC = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AppointmentsLabel
+            // 
+            this.AppointmentsLabel.AutoSize = true;
+            this.AppointmentsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppointmentsLabel.Location = new System.Drawing.Point(30, 9);
+            this.AppointmentsLabel.Name = "AppointmentsLabel";
+            this.AppointmentsLabel.Size = new System.Drawing.Size(120, 20);
+            this.AppointmentsLabel.TabIndex = 0;
+            this.AppointmentsLabel.Text = "Appointments";
+            // 
+            // CustomersLabel
+            // 
+            this.CustomersLabel.AutoSize = true;
+            this.CustomersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomersLabel.Location = new System.Drawing.Point(30, 251);
+            this.CustomersLabel.Name = "CustomersLabel";
+            this.CustomersLabel.Size = new System.Drawing.Size(95, 20);
+            this.CustomersLabel.TabIndex = 1;
+            this.CustomersLabel.Text = "Customers";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(735, 190);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(23, 274);
+            this.dgvCustomers.MultiSelect = false;
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.ReadOnly = true;
+            this.dgvCustomers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCustomers.Size = new System.Drawing.Size(599, 173);
+            this.dgvCustomers.TabIndex = 3;
+            // 
+            // buttonCalender
+            // 
+            this.buttonCalender.Location = new System.Drawing.Point(628, 274);
+            this.buttonCalender.Name = "buttonCalender";
+            this.buttonCalender.Size = new System.Drawing.Size(160, 46);
+            this.buttonCalender.TabIndex = 4;
+            this.buttonCalender.Text = "Calender";
+            this.buttonCalender.UseVisualStyleBackColor = true;
+            // 
+            // buttonReports
+            // 
+            this.buttonReports.Location = new System.Drawing.Point(628, 339);
+            this.buttonReports.Name = "buttonReports";
+            this.buttonReports.Size = new System.Drawing.Size(160, 46);
+            this.buttonReports.TabIndex = 5;
+            this.buttonReports.Text = "Reports";
+            this.buttonReports.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogOut
+            // 
+            this.buttonLogOut.Location = new System.Drawing.Point(628, 401);
+            this.buttonLogOut.Name = "buttonLogOut";
+            this.buttonLogOut.Size = new System.Drawing.Size(160, 46);
+            this.buttonLogOut.TabIndex = 6;
+            this.buttonLogOut.Text = "Log Out";
+            this.buttonLogOut.UseVisualStyleBackColor = true;
+            this.buttonLogOut.Click += new System.EventHandler(this.buttonLogOut_Click);
+            // 
+            // buttonAddA
+            // 
+            this.buttonAddA.Location = new System.Drawing.Point(244, 228);
+            this.buttonAddA.Name = "buttonAddA";
+            this.buttonAddA.Size = new System.Drawing.Size(75, 39);
+            this.buttonAddA.TabIndex = 7;
+            this.buttonAddA.Text = "Add";
+            this.buttonAddA.UseVisualStyleBackColor = true;
+            // 
+            // buttonModifyA
+            // 
+            this.buttonModifyA.Location = new System.Drawing.Point(335, 228);
+            this.buttonModifyA.Name = "buttonModifyA";
+            this.buttonModifyA.Size = new System.Drawing.Size(75, 39);
+            this.buttonModifyA.TabIndex = 8;
+            this.buttonModifyA.Text = "Modify";
+            this.buttonModifyA.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteA
+            // 
+            this.buttonDeleteA.Location = new System.Drawing.Point(428, 228);
+            this.buttonDeleteA.Name = "buttonDeleteA";
+            this.buttonDeleteA.Size = new System.Drawing.Size(75, 39);
+            this.buttonDeleteA.TabIndex = 9;
+            this.buttonDeleteA.Text = "Delete";
+            this.buttonDeleteA.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddC
+            // 
+            this.buttonAddC.Location = new System.Drawing.Point(244, 453);
+            this.buttonAddC.Name = "buttonAddC";
+            this.buttonAddC.Size = new System.Drawing.Size(75, 39);
+            this.buttonAddC.TabIndex = 10;
+            this.buttonAddC.Text = "Add";
+            this.buttonAddC.UseVisualStyleBackColor = true;
+            this.buttonAddC.Click += new System.EventHandler(this.buttonAddC_Click);
+            // 
+            // buttonModifyC
+            // 
+            this.buttonModifyC.Location = new System.Drawing.Point(335, 453);
+            this.buttonModifyC.Name = "buttonModifyC";
+            this.buttonModifyC.Size = new System.Drawing.Size(75, 39);
+            this.buttonModifyC.TabIndex = 11;
+            this.buttonModifyC.Text = "Modify";
+            this.buttonModifyC.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteC
+            // 
+            this.buttonDeleteC.Location = new System.Drawing.Point(428, 453);
+            this.buttonDeleteC.Name = "buttonDeleteC";
+            this.buttonDeleteC.Size = new System.Drawing.Size(75, 39);
+            this.buttonDeleteC.TabIndex = 12;
+            this.buttonDeleteC.Text = "Delete";
+            this.buttonDeleteC.UseVisualStyleBackColor = true;
+            this.buttonDeleteC.Click += new System.EventHandler(this.buttonDeleteC_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.Controls.Add(this.buttonDeleteC);
+            this.Controls.Add(this.buttonModifyC);
+            this.Controls.Add(this.buttonAddC);
+            this.Controls.Add(this.buttonDeleteA);
+            this.Controls.Add(this.buttonModifyA);
+            this.Controls.Add(this.buttonAddA);
+            this.Controls.Add(this.buttonLogOut);
+            this.Controls.Add(this.buttonReports);
+            this.Controls.Add(this.buttonCalender);
+            this.Controls.Add(this.dgvCustomers);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.CustomersLabel);
+            this.Controls.Add(this.AppointmentsLabel);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
--- KyleDelacruzc969/Pages/Main.cs
+++ KyleDelacruzc969/Pages/Main.cs
@@ -37,6 +37,7 @@ namespace KyleDelacruzc969.Pages
 		}
 		public static int customerCount;
 		public static int addressCount = 1;
+		public static DataGridViewRow CustomerIndex;
 
 		private void buttonLogOut_Click(object sender, EventArgs e)
 		{
@@ -77,5 +78,46 @@ namespace KyleDelacruzc969.Pages
 			}
 			AC1.Show();
 		}
-	}
+
+        private void buttonDeleteC_Click(object sender, EventArgs e)
+        {
+			var ID = dgvCustomers.SelectedRows[0].Index;
+			var forDelete = ID + 1;
+
+			string connectionString = ConfigurationManager.ConnectionStrings["MyMySqlKey"].ConnectionString;
+			MySqlConnection con = new MySqlConnection(connectionString);
+
+			con.Open();
+
+			string sqlString1 = "DELETE FROM address WHERE (addressID = " + forDelete + ")";
+			string sqlString = "DELETE FROM customer WHERE (customerId = " + forDelete + ")";
+			MySqlCommand cmd1 = new MySqlCommand(sqlString1, con);
+			MySqlCommand cmd = new MySqlCommand(sqlString, con);
+
+			cmd.ExecuteNonQuery();
+			cmd1.ExecuteNonQuery();
+			con.Close();
+
+
+			string connectionString1 = ConfigurationManager.ConnectionStrings["MyMySqlKey"].ConnectionString;
+			MySqlConnection con2 = new MySqlConnection(connectionString);
+
+			con.Open();
+			string sqlString2 = "SELECT customerName, address, address2, city, country, phone FROM client_schedule.customer Left join client_schedule.address on customer.addressID = address.addressId Left join client_schedule.city on address.cityId = city.cityId Left join client_schedule.country on city.countryId = country.countryId; ";
+
+
+			MySqlCommand cmd2 = new MySqlCommand(sqlString2, con2);
+			MySqlDataAdapter adp2 = new MySqlDataAdapter(cmd2);
+			DataTable customer = new DataTable();
+			adp2.Fill(customer);
+			dgvCustomers.DataSource = customer;
+
+
+
+
+
+
+			
+        }
+    }
 }
