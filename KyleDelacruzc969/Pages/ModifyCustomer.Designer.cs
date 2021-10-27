9b7aac8d35099476644ff71ba688dc1fa850e430
Kyle Delacruz <k.delacruz48.kd@gmail.com>
2021-10-27 02:13:31 +0000

    Add/Modify/Delete customer all work. Still need input validation

--- KyleDelacruzc969/Pages/ModifyCustomer.Designer.cs
+++ KyleDelacruzc969/Pages/ModifyCustomer.Designer.cs
@@ -29,12 +29,186 @@ namespace KyleDelacruzc969.Pages
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "ModifyCustomer";
+            this.comboBoxCountry = new System.Windows.Forms.ComboBox();
+            this.comboBoxCity = new System.Windows.Forms.ComboBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelCountry = new System.Windows.Forms.Label();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelModify = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxCountry
+            // 
+            this.comboBoxCountry.Enabled = false;
+            this.comboBoxCountry.FormattingEnabled = true;
+            this.comboBoxCountry.Items.AddRange(new object[] {
+            "US",
+            "Canada",
+            "Norway"});
+            this.comboBoxCountry.Location = new System.Drawing.Point(131, 214);
+            this.comboBoxCountry.Name = "comboBoxCountry";
+            this.comboBoxCountry.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxCountry.TabIndex = 27;
+            this.comboBoxCountry.SelectedIndexChanged += new System.EventHandler(this.comboBoxCountry_SelectedIndexChanged);
+            // 
+            // comboBoxCity
+            // 
+            this.comboBoxCity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCity.FormattingEnabled = true;
+            this.comboBoxCity.Items.AddRange(new object[] {
+            "New York",
+            "Los Angeles",
+            "Toronto",
+            "Vancouver",
+            "Oslo"});
+            this.comboBoxCity.Location = new System.Drawing.Point(131, 176);
+            this.comboBoxCity.Name = "comboBoxCity";
+            this.comboBoxCity.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxCity.TabIndex = 26;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(138, 310);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 25;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Location = new System.Drawing.Point(57, 310);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(75, 23);
+            this.buttonModify.TabIndex = 24;
+            this.buttonModify.Text = "Modify";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(131, 252);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(121, 20);
+            this.textBoxPhone.TabIndex = 23;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(131, 140);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(121, 20);
+            this.textBoxAddress.TabIndex = 22;
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(31, 255);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(38, 13);
+            this.labelPhone.TabIndex = 21;
+            this.labelPhone.Text = "Phone";
+            // 
+            // labelCountry
+            // 
+            this.labelCountry.AutoSize = true;
+            this.labelCountry.Location = new System.Drawing.Point(31, 217);
+            this.labelCountry.Name = "labelCountry";
+            this.labelCountry.Size = new System.Drawing.Size(43, 13);
+            this.labelCountry.TabIndex = 20;
+            this.labelCountry.Text = "Country";
+            // 
+            // labelCity
+            // 
+            this.labelCity.AutoSize = true;
+            this.labelCity.Location = new System.Drawing.Point(31, 179);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(24, 13);
+            this.labelCity.TabIndex = 19;
+            this.labelCity.Text = "City";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(31, 143);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(45, 13);
+            this.labelAddress.TabIndex = 18;
+            this.labelAddress.Text = "Address";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(131, 99);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(121, 20);
+            this.textBoxName.TabIndex = 17;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(31, 102);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 16;
+            this.labelName.Text = "Name";
+            // 
+            // labelModify
+            // 
+            this.labelModify.AutoSize = true;
+            this.labelModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelModify.Location = new System.Drawing.Point(12, 20);
+            this.labelModify.Name = "labelModify";
+            this.labelModify.Size = new System.Drawing.Size(140, 20);
+            this.labelModify.TabIndex = 15;
+            this.labelModify.Text = "Modify customer";
+            // 
+            // ModifyCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 450);
+            this.Controls.Add(this.comboBoxCountry);
+            this.Controls.Add(this.comboBoxCity);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelCountry);
+            this.Controls.Add(this.labelCity);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelModify);
+            this.Name = "ModifyCustomer";
+            this.Text = "ModifyCustomer";
+            this.Load += new System.EventHandler(this.ModifyCustomer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxCountry;
+        private System.Windows.Forms.ComboBox comboBoxCity;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelCountry;
+        private System.Windows.Forms.Label labelCity;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelModify;
+    }
 }
